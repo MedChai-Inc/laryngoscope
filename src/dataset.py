94eb52869266d158efd6ff341d415ee873c46bfe
cstillman <cstillman@email.wm.edu>
2022-03-24 11:21:47 -0400

    removed JSON dependency

--- src/dataset.py
+++ src/dataset.py
@@ -5,14 +5,6 @@ from PIL import Image
 from torch.utils import data
 from torchvision.datasets.folder import pil_loader
 
-import json
-
-
-def load_json(f):
-    with open(f, 'r') as fp:
-        return json.load(fp)
-
-
 class BDDDataset(data.Dataset):
 
     def __init__(self, root, train=True, transform=None):
@@ -28,13 +20,13 @@ class BDDDataset(data.Dataset):
 
         if self.train:
             label_path = glob.glob(
-                os.path.join(self.root, 'training_masks/*.json'))
+                os.path.join(self.root, 'training_masks/*.jpg'))
             image_dir = os.path.join(self.root, '')
             image_paths = glob.glob(
                 os.path.join(self.root, 'training_images/*.jpg'))
         else:
             label_path = glob.glob(
-                os.path.join(self.root, 'testing_masks/*.json'))
+                os.path.join(self.root, 'testing_masks/*.jpg'))
             image_dir = os.path.join(self.root, '')
             image_paths = glob.glob(
                 os.path.join(self.root, 'testing_images/*.jpg'))
@@ -52,12 +44,7 @@ class BDDDataset(data.Dataset):
         image_path, label_path = self.samples[index]
 
         image = pil_loader(image_path)
-        loaded_json = load_json(label_path)
         
-        for frame in loaded_json['frames']:
-            name = frame['name']
-            if name[name.rfind('/')+1] == image_path[image_path.rfind('/')+1]:
-                label = frame
                 
         if self.transform is not None:
             image = self.transform(image)
