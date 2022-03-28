bf8ac2274cdc6d8007443f0d02d325f1ed6a1cac
cstillman <cstillman@email.wm.edu>
2022-03-28 14:54:08 -0400

    Added methods to be filled in to run a training and testing cycle

--- src/training.py
+++ src/training.py
@@ -1,9 +1,34 @@
 import torch
 import numpy
-import torchvision.datasets as dataset
-import torchvision.transforms as transforms
+import dataset
+from torchvision import transforms
+from torch.utils.data import DataLoader
 
-from helperFunc import load_yaml
+#compose the all transformation
+transform = transforms.Compose([transforms.Resize(64), transforms.ToTensor()])
 
+#define the training dataset and the testing dataset
+training_data = dataset.ETIDAtaset(
+    r'/Users/carsonstillman/data/Epiglottis_Data/', train=True, transform=transform)
 
+test_data = dataset.ETIDataset(
+    r'/Users/carsonstillman/data/Epiglottis_Data/', train=False, transform=transform)
+
+#instantiate the dataloaders
+train_dataloader = DataLoader(training_data, batch_size=64)
+test_dataloader = DataLoader(test_data, batch_size=64)
+
+def train_loop():
+    '''Runs the training loop for the Neural network.'''
+    pass
+
+def test_loop():
+    '''Runs the testing loop for the Neural Network.'''
+    pass
+
+def main():
+    '''The main function'''
+
+if __name__=='__main__':
+    main()
 
